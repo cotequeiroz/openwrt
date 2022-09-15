470721b4eec83eced3682bb181789372e2934dcc
Eneas U de Queiroz <cotequeiroz@gmail.com>
2022-09-15 16:03:53 -0300

    kernel: add missing config symbol, deps to rxrpc
    kmod-rxrpc is currently missing kmod-udptunnel4 and kmod-udptunnel6
    dependencies and the DEFAULT_FQ_PIE symbol.
    
    Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>

--- package/kernel/linux/modules/netsupport.mk
+++ package/kernel/linux/modules/netsupport.mk
@@ -1267,11 +1267,13 @@ define KernelPackage/rxrpc
   KCONFIG:= \
 	CONFIG_AF_RXRPC \
 	CONFIG_RXKAD=m \
-	CONFIG_AF_RXRPC_DEBUG=n
+	CONFIG_AF_RXRPC_DEBUG=n \
+	CONFIG_DEFAULT_FQ_PIE=n
   FILES:= \
 	$(LINUX_DIR)/net/rxrpc/rxrpc.ko
   AUTOLOAD:=$(call AutoLoad,30,rxrpc.ko)
-  DEPENDS:= +kmod-crypto-manager +kmod-crypto-pcbc +kmod-crypto-fcrypt
+  DEPENDS:= +kmod-crypto-manager +kmod-crypto-pcbc +kmod-crypto-fcrypt \
+	    +kmod-udptunnel4 +IPV6:kmod-udptunnel6
 endef
 
 define KernelPackage/rxrpc/description
