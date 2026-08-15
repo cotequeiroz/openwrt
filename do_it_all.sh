433bddf0af6686db276e6e8168c20bbaab6abd43
Eneas U de Queiroz <cotequeiroz@gmail.com>
2026-08-15 17:44:44 -0300

    build: do_it_all.sh: do dirclean between builds
    Something in python host build system is failing when a build from a
    different arch is used.  Force a full dirclean for now.
    
    Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>

--- do_it_all.sh
+++ do_it_all.sh
@@ -17,18 +17,22 @@ for f in ${LIST}; do
   sed -e "s/^\\(CONFIG_VERSION_NUMBER=\\).*/\\1\"${VER}\"/" "diffconfig.$f" >.config
   echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:rm bin/t\* logs/\* staging_dir/pacakges $(date)\007"
   rm -rf bin/t* logs/* staging_dir/packages
-  echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:make -j${NPROC} defconfig $(date)\007"
-  make "-j${NPROC}" defconfig
+  echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:make -kj${NPROC} defconfig $(date)\007"
+  make "-kj${NPROC}" defconfig
   ARCH_PACKAGES=$(sed -ne '/CONFIG_TARGET_ARCH_PACKAGES=/{s/.*=\|\"//g;p}' .config)
   [ "$ARCH_PACKAGES" != "$PREV_ARCH_PACKAGES" ] && {
-    echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:make package/postgresql/host/clean $(date)\007"
-    make package/postgresql/host/clean
-    echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:rm bin/\* build_dir/\* staging_dir/t* $(date)\007"
+
+    echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:make dirclean $(date)\007"
+    make dirclean
+#    echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:make package/postgresql/host/clean $(date)\007"
+#    make package/postgresql/host/clean
+
+    echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:rm bin/\* build_dir/\* staging_dir/t\* staging_dir/packages $(date)\007"
     rm -rf bin/* build_dir/t* staging_dir/t* staging_dir/packages
     PREV_ARCH_PACKAGES=""
   }
-  echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:make -j${NPROC} $(date)\007"
-  make "-j${NPROC}" || {
+  echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:make -kj${NPROC} $(date)\007"
+  make "-kj${NPROC}" || {
     echo -ne "\033]0;${USER}@${HOSTNAME} [$f]:tar logs $(date)\007"
     tar caf "log.$f.tar.zstd" "${TAR_LOG_OPTS[@]}" logs && \
         scp "${SSH_OPTS[@]}" "log.$f.tar.zstd" gateway:/home/equeiroz/src/openwrt/
